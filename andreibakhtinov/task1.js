8ceb59ee379a72df70dd9a7d52b67efe5ea2a39b
vev07 <108584083+vev07@users.noreply.github.com>
2023-08-23 12:20:57 +0200

    add answer

--- andreibakhtinov/task1.js
+++ andreibakhtinov/task1.js
@@ -1,2 +1,25 @@
 // В рамках БД "Соц/сеть"
 // вывести среднее значение раекции незаблокированных юзеров
+db.reactions.aggregate([
+    {
+        $lookup: {
+            from: "users",
+            localField: "author_id",
+            foreignField: "_id",
+            as: "author"
+        }
+    },
+    { $unwind: "$author" },
+    {
+        $match: {
+            "author.is_blocked": { $ne: true }
+        }
+    },
+    {
+        $group: {
+            _id: "null",
+            avg_value: { $avg: "$value" }
+        }
+    }
+]);
+
